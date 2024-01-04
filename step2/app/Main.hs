13b50e30e315cfb960fc683e28be4081f4fcbe8d
Trygve Laugstøl <trygvis@inamo.no>
2024-01-04 16:05:35 +0100

    wip

--- step2/app/Main.hs
+++ step2/app/Main.hs
@@ -8,49 +8,35 @@ import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as BCL
 import qualified Network.HTTP.Client as NH
 import qualified Network.HTTP.Types as NH
-import Network.HTTP.Client.TLS
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 import System.Exit ( exitFailure)
 
-import qualified CVPartner as C
+import qualified CVPartner.API as C
+import qualified CVPartner.Client as C
+import qualified CVPartner.Core as C
+import qualified CVPartner.MimeTypes as C
+import qualified CVPartner.Model as C
 
-
-
-
-getCvpartnerApiKey :: [([Char], [Char])] -> IO BS.ByteString
-getCvpartnerApiKey env = case lookup "CVPARTNER_API_KEY" env of
-        Just k -> return $ BS.pack k
-        _ -> putStrLn "Missing CVPARTNER_API_KEY" >> exitFailure
-
-data CvCtx = CvCtx {
-    cvCtxMgr :: NH.Manager
-  , cvCtxConfig :: C.CVPartnerConfig
-}
 
 main :: IO ()
 main = do
     env <- getEnvironment
-    cvpartnerApiKey <- getCvpartnerApiKey env
+    cvpartnerApiKey <- case lookup "CVPARTNER_API_KEY" env of
+        Just k -> return $ BS.pack k
+        _ -> putStrLn "Missing CVPARTNER_API_KEY" >> exitFailure
 
     mgr <- makeManager cvpartnerApiKey
 
-    config0 <- C.withStdoutLogging =<< C.newConfig
-
-    let config =
-            config0 {
-                C.configHost = BCL.pack "https://scienta.cvpartner.com"
-            }
+    config' <- C.withStdoutLogging =<< C.newConfig
+    let config = config' {
+        C.configHost = BCL.pack "https://scienta.cvpartner.com"
+      }
 
     putStrLn $ "Config: " ++ show config
 
-    let ctx = CvCtx {
-        cvCtxMgr = mgr
-      , cvCtxConfig = config
-    }
-
     let req = C.userSearch (C.Accept C.MimeJSON)
-    res <- C.dispatchMime (cvCtxMgr ctx) (cvCtxConfig ctx) req
+    res <- C.dispatchMime mgr config req
     withSuccess res listUsers
-    putStrLn "yo yo"
   where
     listUsers :: [C.User] -> IO ()
     listUsers [] = return ()
@@ -66,17 +52,18 @@ main = do
 withSuccess :: C.MimeResult res -> (res -> IO ()) -> IO ()
 withSuccess result handler = do
   case C.mimeResult result of
-      Left err -> print err
+      Left err -> do
+        print err
+        exitFailure
       Right value -> handler value
 
 makeManager :: BS.ByteString -> IO NH.Manager
 makeManager bearer = do
   NH.newManager tlsManagerSettings {
-    NH.managerModifyRequest = yeah
+    NH.managerModifyRequest = setAuthorization
   }
     where
-      yeah :: NH.Request -> IO NH.Request
-      yeah req = do
+      setAuthorization req = do
         -- print headers
         return req {
             NH.requestHeaders = headers
@@ -86,11 +73,11 @@ makeManager bearer = do
           headers' = NH.requestHeaders req
           headers = setHeader headers' "Authorization" (BS.append (BS.pack "Bearer ") bearer)
 
-setHeader :: [NH.Header] -> String -> BS.ByteString -> [NH.Header]
-setHeader old key value = new
-  where
-    key' = BS.pack key & CI.mk
-    without = filter isKey old
-    new = without ++ [(key', value)]
-    isKey :: NH.Header -> Bool
-    isKey (k, _) = k /= key'
+      setHeader :: [NH.Header] -> String -> BS.ByteString -> [NH.Header]
+      setHeader old key value = new
+        where
+          key' = BS.pack key & CI.mk
+          without = filter isKey old
+          new = without ++ [(key', value)]
+          isKey :: NH.Header -> Bool
+          isKey (k, _) = k /= key'
